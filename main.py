d253d98002307c61ea3b41164896581a3e78e7b1
denis <denis@gmail.com>
2024-03-24 02:12:39 +0500

    updated main.py and spreadsheet.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from datetime import date
 import telebot
-import gspread
+from spreadsheet import add_expense_to_sheet
 
 bot_token = '7130959835:AAE2_9i0vCj6MO2TX-h2YHFug-pvx8Klx9E'
 googlesheet_id = '1S4ksOdt7L49XS09Dm1xTxzDWSDD7UsfnKoTPeNigYPg'
 bot = telebot.TeleBot('7130959835:AAE2_9i0vCj6MO2TX-h2YHFug-pvx8Klx9E')
-gc = gspread.service_account()
 
-
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message,"Привет, Денис. Введи расход через дефис в виде [Категория-Цена-Комментарий]:")
 
@@ -16,14 +14,11 @@ def send_welcome(message):
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     try:
-        today = date.today().strftime("%d.%m.%Y")
-
         category, price, comment = message.text.split("-")
+        add_expense_to_sheet(googlesheet_id, category, price, comment)
+        today = date.today().strftime("%d.%m.%Y")
         text_message = f'На {today} в таблицу расходов добавлена запись: {category} ценой в {price} тенге, {comment} '
         bot.send_message(message.chat.id, text_message)
-
-        sh = gc.open_by_key(googlesheet_id)
-        sh.sheet1.append_row([today, category, price, comment])
     except:
         bot.send_message(message.chat.id, 'ОШИБКА! Неправильный формат данных!')
 
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,4 +1,8 @@
 import gspread
-gc = gspread.service_account()
-sh = gc.open_by_key('1S4ksOdt7L49XS09Dm1xTxzDWSDD7UsfnKoTPeNigYPg')
-print(sh.sheet1.get('A1:D1'))
+from datetime import date
+
+def add_expense_to_sheet(googlesheet_id, category, price, comment):
+    gc = gspread.service_account()
+    today = date.today().strftime("%d.%m.%Y")
+    sh = gc.open_by_key(googlesheet_id)
+    sh.sheet1.append_row([today, category, price, comment])
